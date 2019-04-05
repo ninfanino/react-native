d74d17d6d1493397a25b090d5dfd7b5ec4e69cc5
ninfan <ninfa_sys@hotmail.com>
2019-04-05 11:42:31 -0600

    AppButton

--- App.js
+++ App.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import AppButton from './application/components/AppButton';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <AppButton
+          bgColor="rgba(252, 197, 0, 1)"
+          title="Login"
+          action={() => console.log(1)}
+          iconName="sign-in"
+          iconSize={32}
+          iconColor="#fff"
+        />
       </View>
     );
   }
