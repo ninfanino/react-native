bbf24a8c50ae64486a6e1942ee4b89131478b2aa
ninfan <ninfa_sys@hotmail.com>
2019-04-25 16:55:52 -0500

    BackgroundImage

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { Text, View } from 'react-native';
 import PreLoader from './application/components/PreLoader';
+import BackgroundImage from './application/components/BackgroundImage';
 
 export default class App extends React.Component {
   render() {
     return (
-      <PreLoader/>
+      <BackgroundImage
+      	source={require('./assets/images/bg.jpg')}
+      />
     );
   }
 }
